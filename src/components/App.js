e1d1366a80c58f7ca89c3f4c6c26e45cda8345b7
arnold-aswan <arnold.aswani@student.moringaschool.com>
2023-07-25 22:01:12 +0300

    used a useEffect to fetch data from api

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([]);
 
+  useEffect(() => {
+    try {
+      fetch("http://localhost:4000/questions")
+        .then((r) => r.json())
+        .then((questions) => setQuestions(questions));
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  const handleDeletedQuestion = (deletedQuestion) => {
+    console.log(deletedQuestion);
+    const updatedQuestions = questions.filter(
+      (question) => question.id !== deletedQuestion.id
+    );
+    setQuestions(updatedQuestions);
+  };
+
+  const handleNewQuestion = (newQuestion) => {
+    console.log(newQuestion);
+    console.log(questions);
+    setQuestions([...questions, newQuestion]);
+  };
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? (
+        <QuestionForm onAddQuiz={handleNewQuestion} />
+      ) : (
+        <QuestionList quizes={questions} onDelete={handleDeletedQuestion} />
+      )}
     </main>
   );
 }
