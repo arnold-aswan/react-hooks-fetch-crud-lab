e2715e1297b0371264fc1ac2a6c39a4ed1456052
arnold-aswan <arnold.aswani@student.moringaschool.com>
2023-07-25 22:02:30 +0300

    added fetch to delete items from api

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,8 +1,22 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, onDelete }) {
+  console.log(question);
   const { id, prompt, answers, correctIndex } = question;
 
+  const handleDelete = () => {
+    // console.log(question);
+    try {
+      fetch(`http://localhost:4000/questions/${question.id}`, {
+        method: "DELETE",
+      })
+        .then((r) => r.json())
+        .then(() => console.log(question))
+        .then(() => onDelete(question));
+    } catch (error) {
+      console.log(error);
+    }
+  };
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
       {answer}
@@ -17,7 +31,7 @@ function QuestionItem({ question }) {
         Correct Answer:
         <select defaultValue={correctIndex}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDelete}>Delete Question</button>
     </li>
   );
 }
