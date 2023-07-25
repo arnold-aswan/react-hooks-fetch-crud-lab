709e3b4b1faec5a86abedb1cd4d45a15f8c8d619
arnold-aswan <arnold.aswani@student.moringaschool.com>
2023-07-25 22:03:18 +0300

    passed props to map & diplay data

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,22 @@
 import React from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+function QuestionList({ quizes, onDelete }) {
+  console.log(quizes);
+  console.log(quizes);
+  // const question = questions.map((question) => (
+  //   <QuestionItem question={question} key={question.id} onDelete={onDelete} />
+  // ));
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      {/* display QuestionItem components here after fetching */}
+      <ul>
+        {quizes.map((item) => (
+          <QuestionItem question={item} key={item.id} onDelete={onDelete} />
+        ))}
+      </ul>
     </section>
   );
 }
